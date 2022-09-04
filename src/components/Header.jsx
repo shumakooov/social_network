d6b43574816a23f1bca7be99e69a155305a40c9b
shumakooov <shumakov.13@yandex.ru>
2022-09-04 13:49:56 +0500

    components

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header className={'header'}>
+            <img
+                src={"https://e7.pngegg.com/pngimages/981/512/png-clipart-avengers-logo-captain-america-thor-hulk-logo-captain-america-marvel-avengers-assemble-avengers.png"} alt={'logo'}/>
+        </header>
+    );
+}
+
+export default Header;
